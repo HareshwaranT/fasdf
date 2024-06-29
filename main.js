62e08873406d7fd8d12ecabc318a4e50cdf8170c
HareshwaranT <107734107+HareshwaranT@users.noreply.github.com>
2024-06-29 14:46:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function phone()
+{
+    window.location = "phone.html"
+}
+
+function bear()
+{
+    window.location = "bear.html"
+}
+
+function pikachu()
+{
+    window.location = "pikachu.html"
+}
+
+function milk()
+{
+    window.location = "milk.html"
+}
